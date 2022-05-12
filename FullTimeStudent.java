3eda2c07dffd10509b01ea83c31c59fe82c0832c
kaputo kalandanya <kaputokalan11@gmail.com>
2022-05-12 13:49:26 +0200

    first commit

--- FullTimeStudent.java
+++ FullTimeStudent.java
@@ -1,2 +1,15 @@
-public class FullTimeStudent extends AttendanceRole {
+
+public class FullTimeStudent extends AttendanceRole{
+
+    private Student student;
+
+    public FullTimeStudent(Student student){
+        this.student = student;
+    }
+
+    @Override
+    protected void attendance() {
+        System.out.println("This is a full-time student!");
+    }
+    
 }
--- PartTimeStudent.java
+++ PartTimeStudent.java
@@ -1,2 +1,14 @@
-public class PartTimeStudent extends UndergraduateStudent {
+public class PartTimeStudent extends AttendanceRole{
+
+    private Student student;
+
+    public PartTimeStudent(Student student){
+        this.student = student;
+    }
+
+    @Override
+    protected void attendance() {
+        System.out.println("This is a part time student!");   
+    }
+    
 }
